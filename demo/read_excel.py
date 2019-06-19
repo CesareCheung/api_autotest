1f1e854e27bf7490006831ffa811a95669bc9b65
zhangweixu <1689719986@qq.com>
2019-06-19 13:36:09 +0800

    更新

--- demo/read_excel.py
+++ demo/read_excel.py
@@ -1,26 +1,28 @@
 import xlrd
-from datetime import datetime,date
+
 
 def read_excel():
     """
     读取Excel文件
     :return:
     """
-    # 文件位置
-    excel_file=xlrd.open_workbook('demo/test.xlsx')
+    # 打开excel文件进行数据提取
+    excel_file = xlrd.open_workbook('demo/test.xlsx')
 
     # 获取Excel文件sheet名
     excel_file.sheet_names()
     print(excel_file.sheet_names())
-    # 若有多个sheet则需要指定读取目标sheet
-    sheet1_name=excel_file.sheet_names()[0]
+    # 有多个sheet时需要指定读取目标sheet
+    sheet1_name = excel_file.sheet_names()[0]
     print(sheet1_name)
     # 获取sheet1内容
+    # 通过index读取
     # sheet1_info = excel_file.sheet_by_index(0)
-    sheet1_info=excel_file.sheet_by_name('Sheet1')
+    # 通过对应的名字读取
+    sheet1_info = excel_file.sheet_by_name('Sheet1')
 
     # 读取Excel文件所有数据
-    values=[]
+    values = []
     for i in range(sheet1_info.nrows):
         print(sheet1_info.row_values(i))
         values.append(sheet1_info.row_values(i))
@@ -29,9 +31,8 @@ def read_excel():
     rows = sheet1_info.row_values(1)
     cols = sheet1_info.col_values(2)
     # 获取表格里的内容，三种方式
-    print(sheet1_info.cell(1, 2).value)   # 获取第二行第三列数据
+    print(sheet1_info.cell(1, 2).value)  # 获取第二行第三列数据
     print(sheet1_info.cell_value(1, 0))
     print(sheet1_info.row(1)[0].value)
 
-    print(rows,cols)
-
+    print(rows, cols)
