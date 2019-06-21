24b322bcbf53e75121816415b1e8faac1d8543e8
zhangweixu <1689719986@qq.com>
2019-06-21 15:56:03 +0800

    更新操作excel方法

--- tool/operation_excel.py
+++ tool/operation_excel.py
@@ -4,4 +4,25 @@ from xlutils.copy import copy
 
 
 class OperationExcel:
-    pass
+    def __init__(self, file_name, sheet_id):
+        """
+        初始化
+        :param file_name: 文件名
+        :param sheet_id:
+        """
+        if file_name:
+            self.file_name = file_name
+            self.sheet_id = sheet_id
+        else:
+            self.file_name = '..dataconfig/case1.xls'
+            self.sheet_id = 0
+        self.data = self.get_data()
+
+    def get_data(self):
+        """
+        获取sheets的内容
+        :return:
+        """
+        data = xlrd.open_workbook(self.file_name)
+        tables = data.sheets()[self.sheet_id]
+        return tables
