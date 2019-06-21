ad04eec0f9c0df4b2e07f71afa521f7111511c94
zhangweixu <zhangweixu1379@163.com>
2019-06-21 11:38:54 +0800

    mock方法封装

---
+++ Base/mock.py
@@ -0,0 +1,9 @@
+import mock
+
+
+# mock方法封装
+
+def mock_test(mock_method, request_data, url, method, response_data):
+    mock_method = mock.Mock(return_value=response_data)
+    res = mock_method(url, method, request_data)
+    return res
