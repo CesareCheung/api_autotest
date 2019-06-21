ec40f628e99bf10f6926fb4a51317b837c0b574d
zhangweixu <1689719986@qq.com>
2019-06-21 19:04:06 +0800

    操作json文件封装

---
+++ tool/operation_json.py
@@ -0,0 +1,44 @@
+import json
+
+
+class OperetionJson:
+    """操作JSON文件"""
+
+    def __init__(self, file_path=None):
+        if file_path == None:
+            self.file_path = '../dataconfig/user.json'
+        else:
+            self.file_path = file_path
+        self.data = self.read_json()
+
+    def read_json(self):
+        """
+        读取JSON数据
+        :return:
+        """
+        with open(self.file_path) as fp:
+            data = json.loads(fp)
+        return data
+
+    def get_data(self, id):
+        """
+        根据关键字获取数据
+        :param id:
+        :return:
+        """
+        print(type(self.data))
+        return self.data[id]
+
+    def write_data(self, data):
+        """
+        写入JSON数据
+        :param data:
+        :return:
+        """
+        with open('../dataconfig/cookie.json', 'w', encoding='utf-8') as fp:
+            fp.write(json.dumps(data))
+
+
+if __name__ == '__main__':
+    operjson = OperetionJson()
+    operjson.get_data('shop')
