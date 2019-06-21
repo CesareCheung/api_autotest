aa777d30f13a3c7537a5e08f4a3ffe58ff604c27
zhangweixu <1689719986@qq.com>
2019-06-21 16:49:35 +0800

    操作excel封装

--- tool/operation_excel.py
+++ tool/operation_excel.py
@@ -26,3 +26,34 @@ class OperationExcel:
         data = xlrd.open_workbook(self.file_name)
         tables = data.sheets()[self.sheet_id]
         return tables
+
+    def get_lines(self):
+        """
+        获取单元格行数
+        :return:
+        """
+        tables = self.data
+        return tables.nrows
+
+    def cell_value(self, row, col):
+        """
+        获取一个单元格内容
+        :param row:行
+        :param col:列
+        :return:
+        """
+        return self.data.cell_value(row, col)
+
+    def write_value(self, row, col, value):
+        """
+        写入数据
+        :param row: 行
+        :param col: 列
+        :param value: 值
+        :return:
+        """
+        read_data = xlrd.open_workbook(self.file_name)
+        write_data = copy(read_data)
+        sheet_data = write_data.get_sheet(0)
+        sheet_data.write(row, col, value)
+        write_data.save(self.file_name)
