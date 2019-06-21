27b798b6fb7b019028787b7fdec909b7705fd283
zhangweixu <zhangweixu1379@163.com>
2019-06-21 11:31:32 +0800

    读取excel方法

--- demo/read_excel.py
+++ demo/read_excel.py
@@ -1,13 +1,13 @@
 import xlrd
 
 
-def read_excel():
+def read_excel(file_path):
     """
     读取Excel文件
     :return:
     """
     # 打开excel文件进行数据提取
-    excel_file = xlrd.open_workbook('demo/test.xlsx')
+    excel_file = xlrd.open_workbook(file_path)
 
     # 获取Excel文件sheet名
     excel_file.sheet_names()
@@ -24,15 +24,22 @@ def read_excel():
     # 读取Excel文件所有数据
     values = []
     for i in range(sheet1_info.nrows):
-        print(sheet1_info.row_values(i))
         values.append(sheet1_info.row_values(i))
+        print(sheet1_info.row_values(i))
 
-    # 获取指定行和列数据
-    rows = sheet1_info.row_values(1)
-    cols = sheet1_info.col_values(2)
-    # 获取表格里的内容，三种方式
-    print(sheet1_info.cell(1, 2).value)  # 获取第二行第三列数据
-    print(sheet1_info.cell_value(1, 0))
-    print(sheet1_info.row(1)[0].value)
 
-    print(rows, cols)
+
+    # # 获取指定行和列数据
+    # rows = sheet1_info.row_values(1)
+    # cols = sheet1_info.col_values(2)
+    # # 获取表格里的内容，三种方式
+    # print(sheet1_info.cell(1, 2).value)  # 获取第二行第三列数据
+    # print(sheet1_info.cell_value(1, 0))
+    # print(sheet1_info.row(1)[0].value)
+    #
+    # print(rows, cols)
+
+
+
+if __name__ == '__main__':
+    read_excel("E:/Test/api_autotest/demo/test.xlsx")
